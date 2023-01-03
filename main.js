c55ff222f3d91c911c4fdbb47cddd071478804f6
foinix0 <fraitalol5@gmail.com>
2023-01-03 12:22:11 +0100

    .

--- main.js
+++ main.js
@@ -1,22 +1,2 @@
 
-
-var slide = ["C:/Users/frait/Documents/GitHub/NetGrong Site/NetGrong/Imm/1.png",
-"C:/Users/frait/Documents/GitHub/NetGrong Site/NetGrong/Imm/2.jpg", 
-" C:/Users/frait/Documents/GitHub/NetGrong Site/NetGrong/Imm/3.jpg"];
-
- var i = 0;
- function demo(){
-    document.getElementById("imms").src = slide[i];
-    i++
-    if(i > 2){
-        i = 0;
-    }
- }
-
- function demo1(){
-    document.getElementById("imms").src = slide[i];
-    i--
-    if( i < 0){
-        i = 2;
-    }
- }
+console.log(screen.width)
