ebd12ee47078efc15e672a5f668a12ea5341a730
foinix0 <fraitalol5@gmail.com>
2023-01-04 14:08:01 +0100

    .

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
 
-console.log(screen.width)
+
+const sliders = document.querySelector(".box")
+var scrcl;
+var scr = 0;
+function sxscroll(){
+    sliders.scrollTo({
+        top: 0,
+        left: (scr -= scrcl),
+        behavior: "smooth"
+    });
+
+    if(scr < 0){
+        scr = 0;
+    }
+
+}
+
+function dxscroll(){
+    if(scr <= sliders.scrollWidth - sliders.clientWidth){
+        sliders.scrollTo({
+            top: 0,
+            left: (scr += scrcl),
+            behavior: "smooth"
+        })
+    }
+
+}
+
+
+ 
