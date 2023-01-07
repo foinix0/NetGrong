685368cd1e447629dae960e746bc5c225efa278a
foinix0 <fraitalol5@gmail.com>
2023-01-07 15:59:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,9 @@
 
 
 var videoid;
-
-
 function Video(id){
-    var emb = document.getElementById("trailer");
-    var clone = emb.cloneNode(true);
-    clone.setAttribute('src', "https://drive.google.com/file/d/" + id +"/preview");
-    emb.parentNode.replaceChild(clone, emb)
+    div = document.getElementById("video");
+    div.innerHTML = '<embed class="trailer" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="https://drive.google.com/file/d/' + id +'/preview"></embed>'
 }
 
 
