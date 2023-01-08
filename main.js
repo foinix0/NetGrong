8f10150c03f9c5d59ca1046feb6a0181cff91ff9
foinix0 <fraitalol5@gmail.com>
2023-01-08 14:36:39 +0100

    .

--- main.js
+++ main.js
@@ -3,5 +3,5 @@
 function reply_click(clickedid){
     localStorage.setItem("id", clickedid);
     window.location.href = "video.html";
-}
+};
 
--- video.js
+++ video.js
@@ -9,7 +9,7 @@ videostring2 = '/preview" bordercolor="#000000" frameborder="0"></iframe>';
 
 switch(id){
 
-
+    //Trailer Auguri
     case "augurimonochino":
         idvideo = "1k7ncwRcPxmTYafkxQKoP0kdA6_kt3FMZ";
         div.innerHTML = videostring1 + idvideo + videostring2;
@@ -22,6 +22,228 @@ switch(id){
         idvideo = "1Lb26SH7by4t_g8WtH6TOazh7I1fGPfRA";
         div.innerHTML = videostring1 + idvideo + videostring2;
         break;    
+    case "auguricardolazzo":
+        idvideo = "1ye3FEyAcERt0LmMUTll10axZwX1IHrh9";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    //Trailer Cimitero Maledetto
+    case "cim1":
+        idvideo = "19FkI-hRxmJyJespuS0ZNbNHIOeqMoqXc";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "cim2":
+        idvideo = "1T2H0qHD2CZb57Gm3qh4fFookIkWDkx1A";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "cim3":
+        idvideo = "1duHR-3wv4sxXmM4mJR7gX8VytW3c1lwg";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "cim4":
+        idvideo = "1h6VufzZuAq3bRESYAU0BnmYmrxT0H1SA";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "cim5":
+        idvideo = "1ofMJZUhyBb5qcfIfyzrxPhLIfTc-v_1q";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break; 
+    case "cimu":
+        idvideo = "1TWaDI19ZNfW57n6g4E2zu1mGb5Axe1cg";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;   
+    case "cim7":
+        idvideo = "1Im1WrA_hAe1cAcfgBaNquIOIY3OHetZG";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;                
+    //Trailer Grong Cinematic Universe
+    case "marvel1":
+        idvideo = "1HZuDCdI5ikBv8J0woXPTyzxsgyP-Y4XM";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel2":
+        idvideo = "1diQDAGwhKzm84sdgrO_EZ6mbk4Pm-o0j";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel3":
+        idvideo = "12Qs8Tb7GwIQ0zUEnn9XPRlmzD3z8jPyQ";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel4":
+        idvideo = "1Z1i07sAi-jzAKGY5ZfpivXL2cWxOJPQu";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel5":
+        idvideo = "1YtU_cVOcFAm7gHOpKZsblM5tRp3nTzbl";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break; 
+    case "marvel6":
+        idvideo = "1qmp0lpeaCaD0g6d1cFma6LShZ0Kgxaa0";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;   
+    case "marvel7":
+        idvideo = "1VuSgtEqNZ2lUQTmw0evoOkTrhJqJxqXk";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;
+    case "marvel8":
+        idvideo = "1HVAIeBnWkhVqHynkmLIeBb9CG9AYmFLL";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel9":
+        idvideo = "1RITo_NFyqYwy7QR9oQfn9_EoL6wjWXEv";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel10":
+        idvideo = "1cTdHKT_SHxFHkFSrp1jcErG-ofbZ_Cca";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel11":
+        idvideo = "1ujsX46V6yGK61cTPfzMJDCkTknYmLr4N";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel12":
+        idvideo = "1yx6_7sKaba-YPkdLduut4s9EncjAGd8q";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break; 
+    case "marvel13":
+        idvideo = "1rIGXI50pr4iAV15O3tovlQZcXWBwXYHw";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;   
+    case "marvel14":
+        idvideo = "1TwfPFQjMRClzqimB5g32B-2J75Feou8z";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;
+    case "marvel15":
+        idvideo = "1spcf7aELghNS-SDe9KoKQaR3IbPW9xN4";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel16":
+        idvideo = "1Yv1P1JkpK0dnmrFCSo2efIZZYcRbNYEW";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel17":
+        idvideo = "1x6CGBgUixRUPh_0ABN7PIbTG1A2sBk68";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel18":
+        idvideo = "1_G_VP48mM5wyOmdYBfdcwZywTHma2k4L";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "marvel19":
+        idvideo = "1dcLT3eirdzfhcM6PI9t6zQl_WKxm9GeN";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break; 
+    case "marvel20":
+        idvideo = "1xl2jLuVzjGMEJDROD3yPZvQTOL_lN4d_";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break; 
+    //Trailer shortfilm
+    case "short1":
+        idvideo = "1LQ39h2i9moHdkCkGO33qkuArC0tezNJg";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "short2":
+        idvideo = "1kkKCqOlpbqcvNl-xavazz2hFiKBfs0Cc";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "short3":
+        idvideo = "1F9ifwbYllCisZ2EMb5si2s5diEivqVfq";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "short4":
+        idvideo = "1aK7PfAh2H_TuuL9TTaByonUaDKS7WvuL";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "short5":
+        idvideo = "1w5Fz77_2LjtOHgzSvc6G-PPfQqhNGq_u";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break; 
+    case "short6":
+        idvideo = "1F5yeU4SsL9A6m_qwcD5tPbVhB7lObjQu";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;   
+    case "short7":
+        idvideo = "1rnzfgJK4vPTBzak77cNtFnOdVf22CfuA";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;
+    case "short8":
+        idvideo = "1pI2mHofiiVrWh4ESBtwKPt6Xixs-I2HF";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "short9":
+        idvideo = "1W8IGzJ82jDOKLdC4FnRlQgMILuQjf6Uv";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "short10":
+        idvideo = "19fkRVXB1QvaV5oicXjWFRuXOqvGXhBgm";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "short11":
+        idvideo = "1_rB2apkvLgxXiE4ZR4vF9LykVIzKgRMU";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "short12":
+        idvideo = "1oK-5cP4YLd8luaghibbHlnaHG5Wqzi9g";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break; 
+    case "short13":
+        idvideo = "1sl-o2TioXUSC3V5PFyeJkCxHPbWS-_cl";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;   
+    case "short14":
+        idvideo = "1LUsfK6tVbdGDpMvFopLSj2cyZiLGjUOR";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;
+    //Trailer goliardie seriali
+    case "seriali1":
+        idvideo = "1VVNhg_JUPXEfgHSQOdhkhMkaOpRaURmR";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "seriali2":
+        idvideo = "1UDzHhSkkrIaURstojQ1Wbo6PeMF-Omx3";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "seriali3":
+        idvideo = "1Z-RhiInZk-8h5a7TPbIbZ7zjHJBTdYsV";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "seriali4":
+        idvideo = "1bT55TKPRk52n0bPjG0Qz0xudlIjI6JRm";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "seriali5":
+        idvideo = "1NTiuUgBCQpB3YtM7Otn_b14M3FixiHqX";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break; 
+    case "seriali6":
+        idvideo = "18nC0hz49uKoY-o9PQFm9mDS-T8FtJg7J";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;   
+    case "seriali7":
+        idvideo = "1cBsGg4uIbVjgMt9r2D5044YWTVRe_e0L";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;
+    case "seriali8":
+        idvideo = "15pD5m5g_5RytQBADG6X-i0SSjg69LOst";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "seriali9":
+        idvideo = "1-vyX47ebNmlK513gldS8wQRtn60gDqVh";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "seriali10":
+        idvideo = "1NSN8t7Digs_GTnKpOr_uchZsAEBnXxiG";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    //Trailer di leondor
+    case "leondor1":
+        idvideo = "1TjdEKuTarocCUhlCqz-mLZ3-1Zz3lIOk";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+    case "leondor2":
+        idvideo = "1-5LR82hHT0PSHQeP1GIOQzraH2m7UvJn";
+        div.innerHTML = videostring1 + idvideo + videostring2;
+        break;  
+
 
 }
 
