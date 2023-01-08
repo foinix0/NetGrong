83983e8a2ac64235111399658dc6dd726fa9abed
foinix0 <fraitalol5@gmail.com>
2023-01-08 13:46:07 +0100

    Update video.js

--- video.js
+++ video.js
@@ -3,7 +3,7 @@
 
 id = localStorage.getItem("id");
 div = document.getElementById("videoview");
-videostring1 = '<iframe style="transform:rotate(90deg); position:fixed; top:25%; left:-59%; bottom:0; right:0; width:220%; height:50%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="https://drive.google.com/file/d/'; 
+videostring1 = '<iframe style="transform:rotate(90deg); position:fixed; top:25%; left:-60%; bottom:0; right:0; width:220%; height:50%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="https://drive.google.com/file/d/'; 
 videostring2 = '/preview" frameborder="0"></iframe>';
 
 
